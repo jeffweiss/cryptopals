18c2c16cce3a5ae54ab6b709d95a76bc93c21bea
Jeff Weiss <jeff.weiss@puppet.com>
2016-04-15 21:54:45 -0700

    refactor, add encryption_oracle

--- set02/challenge11/challenge11.ex
+++ set02/challenge11/challenge11.ex
@@ -1,60 +1,112 @@
-defmodule Challenge10 do
+defmodule Challenge11 do
   use Bitwise
 
   def random_key do
-    :crypto.strong_rand_bytes
+    :crypto.strong_rand_bytes(16)
   end
 
-  def file_contents(filename) do
-    filename
-    |> File.stream!
-    |> Stream.map(&String.strip/1)
-    |> Enum.join
-    |> Base.decode64!
+  def encryption_oracle(plaintext) do
+    key = random_key
+    iv = random_key
+    data = random_data <> plaintext <> random_data
+    case :crypto.rand_uniform(0, 2) do
+      0 -> encrypt_cbc(data, key, iv)
+      1 -> encrypt_ecb(data, key, iv)
+    end
   end
 
-  def run_challenge do
-    file_contents("10.txt") 
+  def encrypt_cbc(data, key, iv) do
+    data
     |> :binary.bin_to_list
-    |> Enum.chunk(16)
-    |> Enum.reduce({<<0>>, ""}, &decrypt/2)
+    |> Enum.chunk(16, 16, [])
+    |> Enum.map(&pkcs7_padding/1)
+    |> Enum.reduce({iv, "", key}, &encrypt/2)
     |> elem(1)
   end
 
-  def decrypt(block, {iv, results}) do
-    plaintext = decrypt(block, iv)
-    {block, results <> plaintext}
+  def encrypt_ecb(data, key, iv) do
+    data
+    |> :binary.bin_to_list
+    |> Enum.chunk(16, 16, [])
+    |> Enum.map(&pkcs7_padding/1)
+    |> Enum.map(&encrypt(&1, iv, key))
+    |> Enum.join
   end
 
-  def decrypt(block, iv) do
-    :crypto.block_decrypt(:aes_ecb, "YELLOW SUBMARINE", block)
+  def decrypt_cbc(data, key, iv) do
+    data
+    |> :binary.bin_to_list
+    |> Enum.chunk(16)
+    |> Enum.map(&pkcs7_padding/1)
+    |> Enum.reduce({iv, "", key}, &decrypt/2)
+    |> elem(1)
+  end
+
+  def decrypt_ecb(data, key, iv) do
+    data
+    |> :binary.bin_to_list
+    |> Enum.chunk(16)
+    |> Enum.map(&pkcs7_padding/1)
+    |> Enum.map(&decrypt(&1, iv, key))
+    |> Enum.join
+  end
+
+  defp random_data do
+    :crypto.rand_uniform(5, 11)
+    |> :crypto.strong_rand_bytes
+  end
+
+  def decrypt(block, {iv, results, key}) do
+    plaintext = decrypt(block, iv, key)
+    {block, results <> plaintext, key}
+  end
+
+  def decrypt(block, iv, key) do
+    :crypto.block_decrypt(:aes_ecb, key, block)
     |> xor_with(iv)
   end
 
-  def encrypt(block, {iv, results}) do
-    ciphertext = encrypt(block, iv)
-    {ciphertext, results <> ciphertext}
-  end
-  def encrypt(block, iv) do
-    cbc_block = xor_with(block, iv)
-    :crypto.block_encrypt(:aes_ecb, "YELLOW SUBMARINE", cbc_block)
+  def encrypt(block, {iv, results, key}) do
+    ciphertext = encrypt(block, iv, key)
+    {ciphertext, results <> ciphertext, key}
   end
 
-  def xor_list(left, right) do
+  def encrypt(block, iv, key) do
+    cbc_block = xor_with(block, iv)
+    :crypto.block_encrypt(:aes_ecb, key, cbc_block)
+  end
+
+  defp xor_list(left, right) do
     zipped = Enum.zip(left, right)
     for {l, r} <- zipped, do: l ^^^ r
   end
 
-  def xor_with(text, key) when is_binary(text), do: xor_with(:binary.bin_to_list(text), key)
-  def xor_with(text, key) when is_binary(key), do: xor_with(text, :binary.bin_to_list(key))
-  def xor_with(text, key) when is_list(text) and is_list(key) do
+  defp xor_with(text, key) when is_binary(text), do: xor_with(:binary.bin_to_list(text), key)
+  defp xor_with(text, key) when is_binary(key), do: xor_with(text, :binary.bin_to_list(key))
+  defp xor_with(text, key) when is_list(text) and is_list(key) do
     key_stream = Stream.cycle(key)
     text
     |> xor_list(key_stream)
     |> :binary.list_to_bin
   end
-end
+  def pkcs7_padding(block, size \\ 16)
+  def pkcs7_padding(block, size) when is_list(block) do
+    remaining = size - length(block)
+    pad_with(block, remaining, remaining)
+  end
+  def pkcs7_padding(block, size) when is_binary(block) do
+    remaining = size - byte_size(block)
+    pad_with(block, remaining, remaining)
+  end
 
-Challenge10.run_challenge
-|> IO.puts
+  defp pad_with(block, _byte, count) when count <= 0, do: block
+  defp pad_with(block, byte, count) when is_list(block) do
+    block ++ [byte]
+    |> pad_with(byte, count - 1)
+  end
+  defp pad_with(block, byte, count) when is_binary(block) do
+    block <> <<byte>>
+    |> pad_with(byte, count - 1)
+  end
+end
 
